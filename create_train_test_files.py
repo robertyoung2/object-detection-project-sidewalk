ef7701b6c3dbcd121a48427f142b9d13b4b107a3
Robert Young <robert.young.ie@gmail.com>
2021-06-04 17:00:49 +0100

    feat: Updated to work correctly as a script
    Added the following functions:
    
    - main(): calls functions in running order.
    - image_train_test_txt(): outputs paths to image files to be used for training and testing purposes. 
    - split_data() - splits data into train and test splits. 
    
    Added if __name__ call.

--- create_train_test_files.py
+++ create_train_test_files.py
@@ -2,30 +2,45 @@ import os
 from sklearn.model_selection import train_test_split
 
 
-def image_file_names(directory_path="/media/robert/1TB HDD/scene_crops/grouped_scene/training"):
+def image_file_names(directory_path="/media/robert/1TB HDD/scene_crops/grouped_scene/all_data"):
 
-    X, y = [], []
+    x, y = [], []
     all_files = os.listdir(directory_path)
-    all_files.sort() # ensure correct ordering
+    all_files.sort()  # ensure correct ordering
     for file in os.listdir(directory_path):
         if file.endswith(".jpg"):
-            X.append(file)
+            x.append(file)
         elif file.endswith(".txt"):
             y.append(file)
+    return x, y
 
-    return X, y
 
+def image_train_test_txt(x_train, x_test):
 
-def image_train_test_txt(X_train, X_test):
     path = "/home/people/06681344/scratch/data/"
 
     with open("train.txt", "w") as output_train:
-        for file_name in X_train:
+        for file_name in x_train:
             output_train.write(path + file_name)
             output_train.write("\n")
 
     with open("test.txt", "w") as output_test:
-        for file_name in X_test:
+        for file_name in x_test:
             output_test.write(path + file_name)
             output_test.write("\n")
 
+
+def split_data(x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+    return x_train, x_test, y_train, y_test
+
+
+def main():
+    x, y = image_file_names()
+    print(len(x), len(y))
+    x_train, x_test, y_train, y_test = split_data(x, y)
+    image_train_test_txt(x_train, x_test)
+
+
+if __name__ == "__main__":
+    main()
