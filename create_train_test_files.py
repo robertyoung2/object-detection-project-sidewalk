3ceaf12bb34ea908d0c239567e725e1dc89a6d31
Robert Young <robert.young.ie@gmail.com>
2021-06-04 10:55:17 +0100

    feat: Added image_train_test_txt() function

--- create_train_test_files.py
+++ create_train_test_files.py
@@ -1,4 +1,5 @@
 import os
+from sklearn.model_selection import train_test_split
 
 
 def image_file_names(directory_path="/media/robert/1TB HDD/scene_crops/grouped_scene/training"):
@@ -13,4 +14,18 @@ def image_file_names(directory_path="/media/robert/1TB HDD/scene_crops/grouped_s
             y.append(file)
 
     return X, y
+
+
+def image_train_test_txt(X_train, X_test):
+    path = "/home/people/06681344/scratch/data/"
+
+    with open("train.txt", "w") as output_train:
+        for file_name in X_train:
+            output_train.write(path + file_name)
+            output_train.write("\n")
+
+    with open("test.txt", "w") as output_test:
+        for file_name in X_test:
+            output_test.write(path + file_name)
+            output_test.write("\n")
 
